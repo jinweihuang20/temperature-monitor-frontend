0c22881f013eed3689a1e58ce2e8cf06af15cbac
jinweihuang20 <jinweihuang1020@gmail.com>
2026-05-06 17:29:07 +0800

    Add GitHub Actions workflow for deploying to GitHub Pages and update Vite config for dynamic base path. This allows for proper deployment of the application based on the repository name when running in GitHub Actions.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
+
 export default defineConfig({
+  base: isGithubActions && repoName ? `/${repoName}/` : '/',
   plugins: [vue()],
 })
